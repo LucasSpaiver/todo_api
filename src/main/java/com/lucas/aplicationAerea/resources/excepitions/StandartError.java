f7e37c5121c022e4654d726dd1103e0181f7f061
Lucas Spavier <lucaspavier@gmail.com>
2025-04-08 15:06:33 -0300

    BackEnd Finalizado

--- src/main/java/com/lucas/aplicationAerea/resources/excepitions/StandartError.java
+++ src/main/java/com/lucas/aplicationAerea/resources/excepitions/StandartError.java
@@ -4,6 +4,10 @@ import java.io.Serializable;
 
 public class StandartError implements Serializable {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	private Long timestamp;
 	private Integer status;
 	private String message;
--- src/main/java/com/lucas/aplicationAerea/services/DBService.java
+++ src/main/java/com/lucas/aplicationAerea/services/DBService.java
@@ -1,13 +1,11 @@
 package com.lucas.aplicationAerea.services;
 
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.lucas.aplicationAerea.domain.Todo;
 import com.lucas.aplicationAerea.repositories.TodoRepository;
 
 @Service
@@ -17,8 +15,6 @@ public class DBService {
 	private TodoRepository todoRepository;
 
 	public void instanciaBD() {
-		DateTimeFormatter formater = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
-		
 		todoRepository.saveAll(Arrays.asList());
 	}
 
